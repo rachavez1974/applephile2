470137f309439da3ee78de3b463cced81ddf906d
Ramon Chavez <rachavez1974@gmail.com>
2019-06-11 18:43:57 -0400

    Add #get_states_names to CraigsList model, and test

--- spec/craigslist.rb
+++ spec/craigslist.rb
@@ -1,8 +1,27 @@
 require "spec_helper"
 require_relative "../lib/craigslist.rb"
 
+
 RSpec.describe "CraigsList" do                  
   let!(:cl_first) {CraigsList.new()}
+  let!(:state_names) { 
+                  ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California',
+                  'Colorado','Connecticut','Delaware','District of Columbia','Florida',
+                  'Georgia','Guam','Hawaii',
+                  'Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana',
+                  'Maine','Marshall Islands','Maryland','Massachusetts','Michigan',
+                  'Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada',
+                  'New Hampshire','New Jersey','New Mexico','New York','North Carolina',
+                  'North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon',
+                  'Palau','Pennsylvania','Rhode Island','South Carolina',
+                  'South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island',
+                  'Virginia','Washington','West Virginia','Wisconsin','Wyoming']
+                }
+  def choose_random_state
+    state_names[rand(0..state_names.length - 1)]
+  end
+
+
 
   after(:each) do 
     CraigsList.class_variable_set(:@@all, [])
@@ -19,4 +38,12 @@ RSpec.describe "CraigsList" do
     
     end
   end
+
+  describe "#get_states_names" do
+    it "it visits the about page of craiglist and collects all the states names of the U.S." do
+      state_names = cl_first.get_states_names
+      expect(state_names).to include(choose_random_state)
+    end
+  end
+
 end
