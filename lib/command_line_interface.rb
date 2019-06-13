765344b8a9030c57a7f952db7db1acd7d9f4fcac
Ramon Chavez <rachavez1974@gmail.com>
2019-06-13 13:43:40 -0400

    Add #display_main_menu to CLI

---
+++ lib/command_line_interface.rb
@@ -0,0 +1,14 @@
+class CLI
+
+  
+  def display_main_menu
+    stars = "************************".colorize(:yellow)
+    puts "#{stars} MAIN MENU #{stars}" 
+    puts "Enter \"scrape\" to scrape.".colorize(:blue)
+    puts "Enter \"exit\" to end program.".colorize(:yellow)
+    puts "What would you like to do?".colorize(:blue)
+    gets.chomp
+  end
+
+  
+end
