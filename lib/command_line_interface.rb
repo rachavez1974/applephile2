b6489518126f7ec6ca6b5d28de4ab6c6ceb8f097
Ramon Chavez <rachavez1974@gmail.com>
2019-06-13 13:45:53 -0400

    Add #get_choice to CLI

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,12 +1,21 @@
 class CLI
 
-  
+  INTRO_MESSAGE = "WELCOME TO CRAIGSLIST SCRAPPER!"
+
   def display_main_menu
     stars = "************************".colorize(:yellow)
     puts "#{stars} MAIN MENU #{stars}" 
     puts "Enter \"scrape\" to scrape.".colorize(:blue)
     puts "Enter \"exit\" to end program.".colorize(:yellow)
     puts "What would you like to do?".colorize(:blue)
+    gets.chomp
+  end
+
+
+  def get_choice
+    puts "Enter number to see link phone on browser.".colorize(:cyan)
+    puts "next for more phones".colorize(:green)
+    puts "Or enter exit to re-scrape.".colorize(:blue)
     gets.chomp
   end
 
